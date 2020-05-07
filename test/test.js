fcecee2c206c96bad3a6664376fd62d4a2d13bb4
Nehhaket <nehhaket@.gmail.com>
2020-05-07 20:50:55 +0200

    add argument type test

--- test/test.js
+++ test/test.js
@@ -3,10 +3,31 @@ const ExpAdder = require('../index')
 
 describe('ExpAdder', function() {
   describe('#add_two()', function() {
-    it('should return empty object when given no params', function() {
+    it('should return empty object when given no arguments', function() {
       const result  = ExpAdder.add_two()
-      const entries = Object.entries(result).length
-      assert.strictEqual(entries, 0)
+      const entries = Object.entries(result)
+      assert.strictEqual(entries.length, 0)
+    })
+
+    it('should throw a TypeError when given wrong type of argument', function() {
+      try { // array
+        ExpAdder.add_two([], [])
+        throw Error
+      } catch(err) {
+        assert.strictEqual(true, err instanceof TypeError)
+      }
+      try { // string
+        ExpAdder.add_two('', '')
+        throw Error
+      } catch(err) {
+        assert.strictEqual(true, err instanceof TypeError)
+      }
+      try { // number
+        ExpAdder.add_two(1, 2)
+        throw Error
+      } catch(err) {
+        assert.strictEqual(true, err instanceof TypeError)
+      }
     })
   })
 })
