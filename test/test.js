ffcc2c356f63721ccd3da33ff351ff9178d4ef89
Nehhaket <nehhaket@.gmail.com>
2020-05-07 21:15:38 +0200

    add test for propper work

--- test/test.js
+++ test/test.js
@@ -3,9 +3,19 @@ const ExpAdder = require('../index')
 
 describe('ExpAdder', function() {
   describe('#add_two()', function() {
+    it('should work propperly for inputs given in example', function() {
+      const result   = ExpAdder.add_two({ 2: 2, 0: 3 }, { 3: 3, 2: 1 })
+      const expected = { 2: 3, 3: 3, 0: 3 }
+      for (key in result) {
+        assert.strictEqual(key in expected, true)
+        assert.strictEqual(expected[key], result[key])
+      }
+    })
+    
     it('should return empty object when given no arguments', function() {
       const result  = ExpAdder.add_two()
       const entries = Object.entries(result)
+      assert.strictEqual(typeof result, 'object')
       assert.strictEqual(entries.length, 0)
     })
 
