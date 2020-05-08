7bf985fa214d39ce31a94c078bd9a4addeadfc3b
Nehhaket <nehhaket@.gmail.com>
2020-05-08 19:04:55 +0200

    add more extensive tests and code refactor

--- index.js
+++ index.js
@@ -3,11 +3,10 @@
  *  add_two
  *
  * arguments:
- *   - expression_1 : math expression
- *   - expression_2 : math expression
+ *   - args : list of math expression
  * 
  * returns:
- *   - math expression; sum of arguments
+ *   - result : sum of arguments; math expression
  *
  * note:
  *   math expressions are represented as a collection of (key: value) pairs,
@@ -16,33 +15,34 @@
  *     `3x^3 + x^2` becomes `{ 3: 3, 1: 2 }`
  * 
  */
-const add_two = (expression_1 = {}, expression_2 = {}) => {
+const add = (...args) => {
 // argument checks
-  if(typeof(expression_1)     !== 'object'
-  || expression_1.constructor !== Object
-  || expression_1.forEach     !== undefined)
-    throw TypeError('wrong type for argument 1')
-  if(typeof(expression_2)     !== 'object'
-  || expression_2.constructor !== Object
-  || expression_2.forEach     !== undefined)
-    throw TypeError('wrong type for argument 2')
-// coefficient checks
-  for(key in expression_1) if(isNaN(parseFloat(expression_1[key])))
-    throw TypeError('Coefficient "' + expression_1[key] + '" is not a number.')
-  for(key in expression_2) if(isNaN(parseFloat(expression_2[key])))
-    throw TypeError('Coefficient "' + expression_2[key] + '" is not a number.')
+  args.forEach((expression, index) => {
+    if(typeof(expression) !== 'object' || expression.constructor !== Object) {
+      throw TypeError('wrong type for argument ' + (index + 1));
+    }
+  });
 
-// for every key in expression_2 check if it exists in expression_1,
-// if it does then incement the value, otherwise insert new
-  for (key in expression_2)
-    if(key in expression_1)
-      expression_1[key] = parseFloat(expression_1[key])
-                        + parseFloat(expression_2[key])
-    else expression_1[key] = expression_2[key]
-  
-  return expression_1
+// main algorithm
+  let result = {};
+  args.forEach((expression, index) => {
+    for(key in expression) {
+      let coefficient = parseFloat(expression[key]);
+      // coefficient check
+      if(isNaN(coefficient)) {
+        let coefficient_type = (expression[key]).constructor.name;
+        throw TypeError('Invalid coefficient type "' + coefficient_type + '" in expression ' + (index + 1));
+      }
+      else if(key in result) {
+        coefficient += result[key];
+      }
+      result[key] = coefficient;
+    }
+  });
+
+  return result;
 }
 
-const ExpAdder = { add_two }
+const ExpAdder = { add };
 
-module.exports = ExpAdder
+module.exports = ExpAdder;
--- test/test.js
+++ test/test.js
@@ -1,52 +1,59 @@
-const assert   = require('assert')
-const ExpAdder = require('../index')
+const assert   = require('assert');
+const ExpAdder = require('../index');
 
 describe('ExpAdder', function() {
-  describe('#add_two()', function() {
+  describe('#add()', function() {
     it('should work propperly for inputs given in example', function() {
-      const result   = ExpAdder.add_two({ 2: 2, 0: 3 }, { 3: 3, 2: 1 })
-      const expected = { 2: 3, 3: 3, 0: 3 }
+      const result   = ExpAdder.add({ 2: 2, 0: 3 }, { 3: 3, 2: 1 });
+      const expected = { 2: 3, 3: 3, 0: 3 };
       for (key in result) {
-        assert.strictEqual(key in expected, true)
-        assert.strictEqual(expected[key], result[key])
+        assert.strictEqual(key in expected, true);
+        assert.strictEqual(expected[key], result[key]);
       }
-    })
+    });
     
     it('should return empty object when given no arguments', function() {
-      const result  = ExpAdder.add_two()
-      const entries = Object.entries(result)
-      assert.strictEqual(typeof result, 'object')
-      assert.strictEqual(entries.length, 0)
-    })
+      const result  = ExpAdder.add();
+      const entries = Object.entries(result);
+      assert.strictEqual(typeof result, 'object');
+      assert.strictEqual(entries.length, 0);
+    });
 
-    it('should throw a TypeError when given wrong type of argument', function() {
-      try { // array
-        ExpAdder.add_two([], [])
-        throw Error
-      } catch(err) {
-        assert.strictEqual(true, err instanceof TypeError)
-      }
-      try { // string
-        ExpAdder.add_two('', '')
-        throw Error
-      } catch(err) {
-        assert.strictEqual(true, err instanceof TypeError)
-      }
-      try { // number
-        ExpAdder.add_two(1, 2)
-        throw Error
-      } catch(err) {
-        assert.strictEqual(true, err instanceof TypeError)
-      }
-    })
+// arguments type test
+    let arguments_test_data = [ [], '', 1 ];
+    arguments_test_data.forEach( (elem) => {
+      it('should throw TypeError when given ' + elem.constructor.name + ' as an argument', function() {
+        try {
+          ExpAdder.add(elem, elem);
+          throw Error;
+        } catch(err) {
+          assert.strictEqual(true, err instanceof TypeError);
+        }
+      });
+    });
 
-    it('should throw TypeError when given wrong type for coefficient', function() {
-      try {
-        ExpAdder.add_two({ 1: 'a', 2: 'b' }, {})
-        throw Error
-      } catch(err) {
-        assert.strictEqual(true, err instanceof TypeError)
-      }
-    })
-  })
-})
+// coefficients type test
+    let coefficients_test_data = [
+      [ 1, '' ],
+      [ 2, [] ],
+      [ 1, {} ]
+    ];
+    coefficients_test_data.forEach(([arg_number, coeff]) => {
+      let coeff_type = coeff.constructor.name;
+      it('should throw TypeError when given ' + coeff_type + ' as a coefficient', function() {
+        try {
+          let args = [];
+          for(let i = 1; i < arg_number; i++) {
+            args.push({});
+          }
+          args.push({ arg_number: coeff });
+          ExpAdder.add(...args);
+          throw Error;
+        } catch(err) {
+          assert.strictEqual(true, err instanceof TypeError);
+          assert.strictEqual(err.message, 'Invalid coefficient type "' + coeff_type + '" in expression ' + arg_number);
+        }
+      });
+    });
+  });
+});
