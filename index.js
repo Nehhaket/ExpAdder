dc812d5dea3f1df8253b7f9050dc7f0f9a92fa93
Nehhaket <nehhaket@.gmail.com>
2020-05-11 13:14:48 +0200

    Remove documentation from index.js

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-/*
- * name: 
- *  sum
- *
- * arguments:
- *   - args : list of math expression
- * 
- * returns:
- *   - result : sum of arguments; math expression
- *
- * note:
- *   math expressions are represented as a collection of (key: value) pairs,
- *   where key is an exponent and value is a coefficient. For ex.:
- *       `2x^2 + 3` becomes `{ 2: 2, 0: 3 }`
- *     `3x^3 + x^2` becomes `{ 3: 3, 1: 2 }`
- * 
- */
 const sum = (...args) => {
 // argument checks
   args.forEach((expression, index) => {
@@ -23,12 +6,12 @@ const sum = (...args) => {
     }
   });
 
-// main algorithm
   let result = {};
+
   args.forEach((expression, index) => {
     for(key in expression) {
       let coefficient = parseFloat(expression[key]);
-      // coefficient check
+    // coefficient check
       if(isNaN(coefficient)) {
         let coefficient_type = (expression[key]).constructor.name;
         throw TypeError('Invalid coefficient type "' + coefficient_type + '" in expression ' + (index + 1));
